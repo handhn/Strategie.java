54d21fb535215f128e2959814440084280787eae
handhn <148367227+handhn@users.noreply.github.com>
2024-11-13 02:06:08 +0100

    Update Strategie.java

--- Strategie.java
+++ Strategie.java
@@ -2,9 +2,9 @@ import java.util.List;
 
 public abstract class Strategie {
     protected String nom;
-    protected List<String> indicateurs;
+    protected IndicateurTechnique indicateurs;
 
-    public Strategie(String nom, List<String> indicateurs) {
+    public Strategie(String nom, IndicateurTechnique indicateurs) {
         this.nom = nom;
         this.indicateurs = indicateurs;
     }
@@ -13,9 +13,5 @@ public abstract class Strategie {
 
     public String getNom() {
         return nom;
-    }
-
-    public List<String> getIndicateurs() {
-        return indicateurs;
     }
 }
