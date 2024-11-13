d930b49c428fde5eb2de072e7438e507cb5ee26b
handhn <148367227+handhn@users.noreply.github.com>
2024-11-13 16:53:22 +0100

    Update StrategieRSI.java

--- StrategieRSI.java
+++ StrategieRSI.java
@@ -7,13 +7,20 @@ public class StrategieRSI extends Strategie {
         super("Stratégie RSI", indicateurs);
     }
 
+
     @Override
     public String executer(List<Double> prices) {
+        // Cette méthode pourrait lever une exception si elle n'est pas supportée
+        throw new UnsupportedOperationException("Cette stratégie nécessite des prix de clôture.");
+    }
+
+    @Override
+    public String executer(List<Double> closingPrices) {
         // Étape 1 : Calculer le RSI actuel
-        double rsi = (double) IndicateurTechnique.createRSI(prices, PERIOD).getValeur();
+        double rsi = (double) IndicateurTechnique.createRSI(closingPrices, PERIOD).getValeur();
         
         // Étape 2 : Calculer le RSI précédent
-        double previousRSI = (double) IndicateurTechnique.createRSI(prices.subList(0, prices.size() - 1), PERIOD).getValeur();
+        double previousRSI = (double) IndicateurTechnique.createRSI(closingPrices.subList(0, prices.size() - 1), PERIOD).getValeur();
 
         // Étape 3 : Vérifier les conditions d'achat
         if (previousRSI <= OVERSOLD && rsi > OVERSOLD) {
